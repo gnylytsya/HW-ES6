d7018d6e4202fc9838db3d7d978f3fb932f7afe8
gnylytsya <gnylytsya@gmail.com>
2017-11-24 17:05:47 +0200

    change modules_with_name.js add computed_property.js

--- modules_with_name.js
+++ modules_with_name.js
@@ -1,23 +1,27 @@
 
  exports.PI = 3.141592;
 
- var _sqrt = function(s, x, last){
-   return x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x;
+ var _sqrt (s, x, last){
+    x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x;
  };
- exports.sqrt = function(s){
-   return _sqrt(s, s/2.0, 0.0);
+ exports.sqrt (s){
+    _sqrt(s, s/2.0, 0.0);
  };
- exports.square = function(x) {
-   return x * x;
+ exports.square (x) {
+    x * x;
  };
 
 
-    var arg1 = process.argv[2];
-    var arg2 = process.argv[3];
-    var PI = require('./Math').PI;
-    var sqrt = require('./Math').sqrt;
-    var square = require('./Math').square;
 
-    console.log(PI);
-    console.log(sqrt(+arg1));
-    console.log(square(+arg2));
+
+
+
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
+var PI = require('./Math').PI;
+var sqrt = require('./Math').sqrt;
+var square = require('./Math').square;
+
+console.log(PI);
+console.log(sqrt(+arg1));
+console.log(square(+arg2));
